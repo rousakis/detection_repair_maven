a57b602b3fd409fec8108c5ab922627d3acb198a
Yannis Roussakis <rousakis@ics.forth.gr>
2016-01-13 17:14:15 +0200

    Updated Changes Ontology Schema files with comments

--- src/main/java/clients/TestDeleteCC.java
+++ src/main/java/clients/TestDeleteCC.java
@@ -10,6 +10,7 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -22,18 +23,30 @@ public class TestDeleteCC {
 
         // test delete cc service
         System.out.println("Testing Delete Service...");
-        String ip = "139.91.183.48";
-//        ip = "localhost";
-        String url = "http://" + ip + ":8181/ForthMaven-1.0/diachron/complex_change";
+        String ip = "139.91.183.48:8181";
+        ip = "139.91.183.40:8080";
+        String url = "http://" + ip + "/ForthMaven-1.0/diachron/complex_change";
         String ccName = "Mark_as_Obsolete_v2";
-        MultivaluedMap<String, String> params = new MultivaluedMapImpl();
-        params.putSingle("name", ccName);
-        params.putSingle("dataset_uri", "http://www.ebi.ac.uk/efo/");
-        WebResource r = c.resource(url);
-        ClientResponse response = r.queryParams(params).accept(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
-        System.out.println(response.getEntity(String.class));
-        System.out.println(response.getStatus());
-        System.out.println("-----\n");
+//        MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+//        params.putSingle("name", ccName);
+//        params.putSingle("dataset_uri", "http://test");
+//        WebResource r = c.resource(url);
+//        ClientResponse response = r.queryParams(params).accept(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
+//        System.out.println(response.getEntity(String.class));
+//        System.out.println(response.getStatus());
+//        System.out.println("-----\n");
         ////
+        String datasetUri;
+        for (int i = 0; i < 200; i++) {
+            datasetUri = "http://test/" + i;
+            MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+            params.putSingle("name", ccName);
+            params.putSingle("dataset_uri", datasetUri);
+            WebResource r = c.resource(url);
+            ClientResponse response = r.queryParams(params).accept(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
+            System.out.println(datasetUri + "\tStatus: " + response.getStatus());
+            System.out.println("-----");
+        }
+
     }
 }
